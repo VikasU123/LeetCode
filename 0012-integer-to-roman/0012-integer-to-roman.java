1c10f4d4d83bc8528241a4b7d3b359c080c1b9db
Vikas U <106076457+VikasU123@users.noreply.github.com>
2023-07-26 08:32:49 +0530

    Time: 6 ms (60.07%), Space: 42.8 MB (99.23%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,9 +1,27 @@
+// class Solution {
+//     public String intToRoman(int num) {
+//         String[] onse={"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+//         String[] tens={"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+//         String[] hend={"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+//         String[] thu={"", "M", "MM", "MMM"};
+//         return (thu[num/1000] + hend[(num%1000)/100] + tens[(num%100)/10] + onse[(num%10)]);
+//     }
+// }
+
 class Solution {
     public String intToRoman(int num) {
-        String[] onse={"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        String[] tens={"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        String[] hend={"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        String[] thu={"", "M", "MM", "MMM"};
-        return (thu[num/1000] + hend[(num%1000)/100] + tens[(num%100)/10] + onse[(num%10)]);
+        String[] s={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int[] values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String t="";
+       int i=0;
+       while(num>0){
+           if(num>=values[i]){
+               t+=s[i];
+               num-=values[i];
+           }
+            else
+           i++;
+       }
+        return t;
     }
 }
